17c1eeb41166c9724c5cf0cdbaccafa203b5d89e
gosia <wheresmyredpillmorpheus@gmail.com>
2023-03-08 16:46:14 +0100

    [change] interface with implementation - polymorphism

---
+++ src/main/java/pl/sda/orange2/lambda/ChlopaiTezPlacza.java
@@ -0,0 +1,12 @@
+package pl.sda.orange2.lambda;
+
+public class ChlopaiTezPlacza implements BoysBoCry{
+    @Override
+    public void sinoreki() {
+        System.out.println("Sina ręka ma!");
+    }
+
+    public void sing() {
+        System.out.println("LA LA LA");
+    }
+}
--- src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
+++ src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
@@ -3,5 +3,13 @@ package pl.sda.orange2.lambda;
 public class LambdaExercises {
     public static void main(String[] args) {
 
+        BoysBoCry boysBoCry = new ChlopaiTezPlacza();
+        boysBoCry.sinoreki();
+
+        ChlopaiTezPlacza boysBoCry2 = new ChlopaiTezPlacza();
+        boysBoCry2.sing();
+
+
+
     }
 }
