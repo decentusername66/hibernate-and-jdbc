1dc6a515a3bc557db278a65f254a51487d846129
gosia <wheresmyredpillmorpheus@gmail.com>
2023-03-08 13:41:34 +0100

    [change] more output to console

--- src/main/java/jah/Main.java
+++ src/main/java/jah/Main.java
@@ -3,5 +3,6 @@ package jah;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Działa...");
+        System.out.println("JDBC and Hibernate... love it!");
     }
 }
